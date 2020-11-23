c1a5c0d1cc271151a5dd51fddea4d1da0028344e
aviv <kaazz931@gmail.com>
2020-11-23 12:54:45 +0200

    kestrel

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -18,6 +19,10 @@ namespace WebApplication
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseKestrel(set => { set.Listen(IPAddress.Parse("0.0.0.0"), 5000); });
+                });
     }
 }
